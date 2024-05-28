80541b37aa7316e1cc9e813f24b6c6f67953c3d5
Ali Noori <alinoorimlm@yahoo.com>
2024-05-28 18:10:59 +0330

    feat: add Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="bg-primary px-6 py-4 flex justify-between items-center">
+      <h1 className="text-white font-bold text-lg flex-1 text-center">
+        Movie Search Application
+      </h1>
+    </header>
+  );
+}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "./header";
 
 export const Layout = function () {
   return (
     <div>
-      {/* <Header /> */}
+      <Header />
       <main>
         <Outlet />
       </main>
